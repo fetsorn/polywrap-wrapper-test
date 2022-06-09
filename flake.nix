dea8488890a4bf51ac80c5d7e2217c2858e409c9
Anton Davydov <fetsorn@gmail.com>
2022-06-09 21:57:43 +0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "polywrap wrapper test";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = inputs@{ self, nixpkgs }:
+    let
+      eachSystem = systems: f:
+        let
+          op = attrs: system:
+            let
+              ret = f system;
+              op = attrs: key:
+                let
+                  appendSystem = key: system: ret: { ${system} = ret.${key}; };
+                in attrs // {
+                  ${key} = (attrs.${key} or { })
+                    // (appendSystem key system ret);
+                };
+            in builtins.foldl' op attrs (builtins.attrNames ret);
+        in builtins.foldl' op { } systems;
+      defaultSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in eachSystem defaultSystems (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages = { };
+        defaultPackage = { };
+        defaultApp = { };
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.nodejs pkgs.yarn ]; };
+      });
+}
