b2c52179c23af2f489bedf963660c519a5bd103a
Anton Davydov <fetsorn@gmail.com>
2022-06-15 04:01:11 +0400

    mutation-functions

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -1,7 +1,9 @@
 import {
   Ethereum_Mutation,
+  Ipfs_Mutation,
   Input_setData,
-  Input_deployContract
+  Input_setIpfsData,
+  SetIpfsDataResult,
 } from "./w3";
 import { abi, bytecode } from "../contracts/SimpleStorage";
 
@@ -23,4 +25,25 @@ export function deployContract(input: Input_deployContract): string {
     args: null,
     connection: input.connection
   }).unwrap();
+}
+
+export function setIpfsData(input: Input_setIpfsData): SetIpfsDataResult {
+  // 1. Upload the data to IPFS
+  const ipfsHash = Ipfs_Mutation.addFile({
+    data: String.UTF8.encode(input.options.data),
+  }).unwrap();
+
+  // 2. Add the data's IPFS hash to SimpleStorage using `setHash(...)`
+  const txReceipt = Ethereum_Mutation.callContractMethodAndWait({
+    address: input.options.address,
+    method: 'function setHash(string value)',
+    args: [ipfsHash],
+    connection: input.connection
+  }).unwrap();
+
+  // 3. Return the result
+  return {
+    ipfsHash,
+    txReceipt: txReceipt.transactionHash,
+  };
 }
