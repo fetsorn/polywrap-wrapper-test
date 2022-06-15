5487f7b51e1877e8736836c707230b00d401f55e
Anton Davydov <fetsorn@gmail.com>
2022-06-15 04:07:44 +0400

    adding-query-functions

--- src/query/index.ts
+++ src/query/index.ts
@@ -1,6 +1,8 @@
 import {
   Ethereum_Query,
-  Input_getData
+  Ipfs_Query,
+  Input_getData,
+  Input_getIpfsData
 } from "./w3";
 
 export function getData(input: Input_getData): u32 {
@@ -12,4 +14,17 @@ export function getData(input: Input_getData): u32 {
   }).unwrap();
 
   return U32.parseInt(res);
+}
+
+export function getIpfsData(input: Input_getIpfsData): string {
+  const hash = Ethereum_Query.callContractView({
+    address: input.address,
+    method: 'function getHash() view returns (string)',
+    args: null,
+    connection: input.connection
+  }).unwrap();
+
+  return String.UTF8.decode(
+    Ipfs_Query.catFile({ cid: hash }).unwrap()
+  );
 }
